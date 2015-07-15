8748e2ae17c6f14d548df53de35a9e57dd1e0af6
Shawn <2perdo@gmail.com>
2015-07-14 23:32:07 -0700

    Added model validation

--- working.py
+++ working.py
@@ -8,27 +8,67 @@ from sklearn.linear_model import SGDRegressor
 from sklearn import decomposition, pipeline, metrics, grid_search
 from sklearn.svm import SVR
 from sklearn.decomposition import TruncatedSVD
+from sklearn import cross_validation
+from sklearn.pipeline import Pipeline, FeatureUnion
 
-# normalized gini by 0x0FFF
-def gini(solution, submission):
-    df = zip(solution, submission)
-    df = sorted(df, key=lambda x: (x[1],x[0]), reverse=True)
-    rand = [float(i+1)/float(len(df)) for i in range(len(df))]
-    totalPos = float(sum([x[0] for x in df]))
-    cumPosFound = [df[0][0]]
-    for i in range(1,len(df)):
-        cumPosFound.append(cumPosFound[len(cumPosFound)-1] + df[i][0])
-    Lorentz = [float(x)/totalPos for x in cumPosFound]
-    Gini = [Lorentz[i]-rand[i] for i in range(len(df))]
-    return sum(Gini)
+class CustomPipeline(object):
+    @classmethod
+    def get_pipeline(cls):
+        pipe_clf = Pipeline([
+        ('svd', TruncatedSVD(n_components=500)),
+        ('sgd', SGDRegressor())
+        ])
+        return pipe_clf
+    
 
-def normalized_gini(solution, submission):
-    normalized_gini = gini(solution, submission)/gini(solution, solution)
+def gini(list_of_values):
+  sorted_list = sorted(list(list_of_values))
+  height, area = 0, 0
+  for value in sorted_list:
+    height += value
+    area += height - value / 2.
+  fair_area = height * len(list_of_values) / 2
+  return (fair_area - area) / fair_area
+  
+def normalized_gini(y_pred, y):
+    normalized_gini = gini(y_pred)/gini(y)
     return normalized_gini
+    
+def validate_model(df, y, p):
+    df = df[0:int(df.shape[0] * p)]
+    cv = cross_validation.KFold(df.shape[0], 10)
+    results = []
+    for traincv, testcv in cv:
+        train_ = df[traincv]
+        test_ = df[testcv]
+        y_train = y.values[traincv]
+        y_test = y.values[testcv]
+    
+        model = CustomPipeline.get_pipeline()
+        model.fit(train_, y_train)
+        
+        results.append(normalized_gini(model.predict(test_), y_test))
+        
+    print "final score: %s with std %s" %(str(np.mean(results)), str(np.std(results)),)
+ 
+
+def test_model_holdout(df, y, p):
+    train_ = df[0:int(df.shape[0] * p)]
+    test_ = df[int(df.shape[0] * p):]
+    
+    y_train = y[0:int(y.shape[0] * p)]
+    y_test = y[int(y.shape[0] * p):]
+    
+    model = CustomPipeline.get_pipeline()
+    model.fit(train_, y_train)
+    
+    print normalized_gini(model.predict(test_), y_test)
+    
     
 
 train = pd.read_csv('data/train.csv')
 train = train[0: int(train.shape[0] * 1)]
+
 test = pd.read_csv('data/test.csv')
 result = pd.read_csv('data/sample_submission.csv')
 y = train.Hazard
@@ -49,12 +89,20 @@ ohc = preprocessing.OneHotEncoder()
 train = ohc.fit_transform(train)
 test = ohc.transform(test)
 
-svd = TruncatedSVD(n_components=500, random_state=42)
-train = svd.fit_transform(train)
-test = svd.transform(test)
+########
+validate_model(train, y, .75)
 
-model = SGDRegressor()
-model.fit(train, y)
+test_model_holdout(train, y, .75)
 
-result.Hazard = model.predict(test)
-result.to_csv('output.csv', index=False)
+
+########
+
+#svd = TruncatedSVD(n_components=500, random_state=42)
+#train = svd.fit_transform(train)
+#test = svd.transform(test)
+
+#model = SGDRegressor()
+#model.fit(train, y)
+
+#result.Hazard = model.predict(test)
+#result.to_csv('output.csv', index=False)
