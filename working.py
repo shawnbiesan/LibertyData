5f70555b5af7e4e06f5485dcb0cdec3d760a8572
Shawn <2perdo@gmail.com>
2015-08-04 15:46:47 -0400

    neural now part of ensemble

--- working.py
+++ working.py
@@ -3,18 +3,22 @@
 import pandas as pd
 import numpy as np
 from numpy import dtype
-from sklearn import preprocessing
 from sklearn.linear_model import SGDRegressor, Lasso, LinearRegression
-from sklearn import decomposition, pipeline, metrics, grid_search
+from sklearn import decomposition, pipeline, metrics, grid_search, preprocessing, cross_validation
 from sklearn.svm import SVR, LinearSVR
 from sklearn.decomposition import TruncatedSVD
-from sklearn import cross_validation
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.metrics import make_scorer
-from sklearn import grid_search
 from sklearn.manifold import TSNE
 import xgboost as xgb
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+from lasagne.nonlinearities import softmax
+from lasagne import layers
+
+
+
 
 one_hot_columns = ['T1_V4', 'T1_V5', 'T1_V6', 'T1_V7', 'T1_V8',
                    'T2_V11', 'T2_V12', 'T2_V13', 'T1_V9', 'T1_V11', 'T1_V12',
@@ -22,31 +26,58 @@ one_hot_columns = ['T1_V4', 'T1_V5', 'T1_V6', 'T1_V7', 'T1_V8',
 
 class CustomPipeline(object):
     @classmethod
-    def get_pipeline(cls):
+    def get_pipeline(cls, with_svd=False):
         pipe_clf = Pipeline([
-        #('svd', TruncatedSVD(n_components=200)),
+        #('poly', preprocessing.PolynomialFeatures()),
+        #('svd', TruncatedSVD(n_components=100)),
         ('mod', SGDRegressor())
-        #('svr', RandomForestRegressor(n_estimators=10, n_jobs=-1))
         ])
         return pipe_clf
     
     @classmethod
-    def get_pipeline_svr(cls):
+    def get_pipeline_svr(cls, with_svd=False):
         pipe_clf = Pipeline([
-        #('svd', TruncatedSVD(n_components=200)),
+        #('poly', preprocessing.PolynomialFeatures()),
+        #('svd', TruncatedSVD(n_components=100)),
         ('mod', LinearSVR(C=1, epsilon=.01))
         ])
         return pipe_clf    
     
     @classmethod
-    def get_pipeline_tree(cls):
+    def get_pipeline_rf(cls, with_svd=False):
         pipe_clf = Pipeline([
-        #('svd', TruncatedSVD(n_components=200)),
-        #('mod', GradientBoostingRegressor(n_estimators=300))
+        #('poly', preprocessing.PolynomialFeatures()),
+        #('svd', TruncatedSVD(n_components=100)),
+        ('mod', RandomForestRegressor(n_estimators=50, n_jobs=-1))
+        ])
+        return pipe_clf
+
+    @classmethod
+    def get_pipeline_tree(cls, with_svd=False):
+        pipe_clf = Pipeline([
+        #('poly', preprocessing.PolynomialFeatures()),
+        #('svd', TruncatedSVD(n_components=50)),
         ('mod', xgb.XGBRegressor(n_estimators=300, min_child_weight=8,
                                  subsample=.5, max_depth=3, learning_rate=.111)),
         ])
         return pipe_clf
+
+    @classmethod
+    def get_pipeline_neural(cls, with_svd=False):
+        layers_s = [('input', layers.InputLayer),
+           ('dense0', layers.DenseLayer),
+           ('output', layers.DenseLayer)
+
+        ]
+
+        pipe_clf = Pipeline([
+        ('mod',NeuralNet(layers=layers_s, input_shape=(None, train.shape[1]),
+                         dense0_num_units=43, output_num_units=1, output_nonlinearity=None,
+                         regression=True, update=nesterov_momentum, update_learning_rate=0.001,
+                         update_momentum=0.9, eval_size=0.2, verbose=0, max_epochs=100)),
+        ])
+        return pipe_clf
+
 
 def Gini(y_true, y_pred):
     # check and get number of samples
@@ -130,12 +161,17 @@ def test_model_holdout(df, y, p, clf):
     
    # model = CustomPipeline.get_pipeline()
     #model.fit(train_, y_train)
+    print train_.shape, y_train.shape
+    print train
     clf.fit(train_, y_train)
-    
-    print "model holdout value: " + str(Gini(y_test, clf.predict(test_)))
+
+    pred = clf.predict(test_)
+    pred = np.reshape(pred, (pred.shape[0]))
+    print y_test.shape, pred.shape
+    print "model holdout value: " + str(Gini(y_test, pred))
     print 
     return clf
-    
+
     
 if __name__ == '__main__':
     
@@ -145,15 +181,12 @@ if __name__ == '__main__':
     test = pd.read_csv('data/test.csv')
     result = pd.read_csv('data/sample_submission.csv')
     y = train.Hazard
+    y = y.astype(np.float32)
     train.drop('Hazard', axis=1, inplace=True)
     train.drop('Id', axis=1, inplace=True)
     test.drop('Id', axis=1, inplace=True)
     
     for col in train.columns:
-        #if train[col].dtype == dtype('O'):
-            #print "transforming col %s" %(col,)
-            #train = pd.concat([train, pd.get_dummies(train[col], prefix=col)])
-            #train.drop(col, axis=1, inplace=True)
         if train[col].dtype == dtype('O'):
             print "transforming col %s" %(col,)
             lbl = preprocessing.LabelEncoder()
@@ -161,36 +194,54 @@ if __name__ == '__main__':
             train[col] = lbl.transform(train[col])
             test[col] = lbl.transform(test[col])
     ohc = preprocessing.OneHotEncoder(sparse=False)
-    train_hot = ohc.fit_transform(train)
-    test_hot = ohc.transform(test)
+    #train_hot = ohc.fit_transform(train)
+    scaler = preprocessing.StandardScaler()
+    train = scaler.fit_transform(train)
+    train = train.astype(np.float32)
+    test = scaler.fit_transform(test)
+    test = test.astype(np.float32)
+    
+
+    #train = pd.concat([train, pd.DataFrame(tmp_train)], axis=1)
+    #train.drop(one_hot_columns, axis=1, inplace=True)
+
+    #test_hot = ohc.transform(test)
+    #test = pd.concat([test, pd.DataFrame(tmp_test)], axis=1)
+    #test.drop(one_hot_columns, axis=1, inplace=True)
+
     
     ########
     #get_optimal_params(train, y, .75, CustomPipeline.get_pipeline_tree())
 
    # validate_model_func(train, y, .75)
 
-    ## todo, allow model 1 to use diff data, and also in general look at which ones should
-    ## actually be ordinal
-    model1 = test_model_holdout(train_hot, y, .75, CustomPipeline.get_pipeline())
-    model2 = test_model_holdout(train, y, .75, CustomPipeline.get_pipeline_tree())
-    model3 = test_model_holdout(train_hot, y, .75, CustomPipeline.get_pipeline_svr())
+    ## todo, allow model 1 to use diff data, and also in general look at which ones should actually be ordinal
 
-    model_list = [model1, model2, model3]
+    model1 = test_model_holdout(train, y, .75, CustomPipeline.get_pipeline_tree())
+    #model2 = test_model_holdout(train, y, .75, CustomPipeline.get_pipeline_tree())
+    model3 = test_model_holdout(train, y, .75, CustomPipeline.get_pipeline_neural())
+    model4 = test_model_holdout(train, y, .75, CustomPipeline.get_pipeline_rf())
 
-    #stacked_predictions_train = pd.concat([pd.DataFrame(model.predict(train)) for i, model in enumerate(model_list) if i != 0 else ], axis=1)
-    stacked_predictions_train = pd.concat([pd.DataFrame(model1.predict(train_hot)),
-                                 pd.DataFrame(model2.predict(train)),
-                                 pd.DataFrame(model3.predict(train_hot)),
+    model_list = [model1, model3, model4]
+
+    #stacked_predictions_train = pd.concat([pd.DataFrame(model.predict(train)) for model in model_list], axis=1)
+    stacked_predictions_train = pd.concat([pd.DataFrame(model1.predict(train)),
+                                 #pd.DataFrame(model2.predict(train)),
+                                 pd.DataFrame(model3.predict(train)),
+                                 pd.DataFrame(model4.predict(train)),
                                 ], axis=1)
+    stacked_predictions_train.columns = ['model' + str(i+1) for i in range(len(model_list))]
+
     stacked_model = test_model_holdout(stacked_predictions_train, y, .75, LinearRegression())
     
-   # stacked_predictions_test = pd.concat([pd.DataFrame(model.predict(test)) for model in model_list], axis=1)
-    stacked_predictions_test = pd.concat([pd.DataFrame(model1.predict(test_hot)),
-                                 pd.DataFrame(model2.predict(test)),
-                                 pd.DataFrame(model3.predict(test_hot)),
+    #stacked_predictions_test = pd.concat([pd.DataFrame(model.predict(test)) for model in model_list], axis=1)
+    stacked_predictions_test = pd.concat([pd.DataFrame(model1.predict(test)),
+                                 #pd.DataFrame(model2.predict(test)),
+                                 pd.DataFrame(model3.predict(test)),
+                                 pd.DataFrame(model4.predict(test)),
                                 ], axis=1)
+    stacked_predictions_test.columns = ['model' + str(i+1) for i in range(len(model_list))]
             
 
     result.Hazard = stacked_model.predict(stacked_predictions_test)
-    #result.Hazard = model2.predict(test)
     result.to_csv('output.csv', index=False)
